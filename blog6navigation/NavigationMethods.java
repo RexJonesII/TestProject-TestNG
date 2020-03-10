ba973a6389815006fb0a28180fbc0f3677657df2
Rex Jones II <Rex.Jones@Test4Success.org>
2020-03-10 11:08:22 -0500

    Add files via upload

---
+++ blog6navigation/NavigationMethods.java
@@ -0,0 +1,60 @@
+package testproject.blog6navigation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class NavigationMethods {
+	WebDriver driver;
+
+	@BeforeClass
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Rex Allen Jones II\\Downloads\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void demoNavigationMethods() {
+		driver.navigate().to("https://blog.testproject.io/");
+		System.out.println("To Title: " + driver.getTitle());
+
+		driver.findElement(By.id("menu-item-7501")).click();
+		driver.findElement(By.id("username")).sendKeys("Rex.Jones@Test4Success.org");
+		driver.navigate().refresh();
+		System.out.println("Refresh Title: " + driver.getTitle());
+
+		driver.navigate().back();
+		System.out.println("Back Title: " + driver.getTitle());
+
+		driver.navigate().forward();
+		System.out.println("Forward Title: " + driver.getTitle());
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
